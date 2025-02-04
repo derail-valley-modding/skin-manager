d1fbc8341a959e26a7596b424f7dcc85abb8867d
Katy Fox <katycat5e@gmail.com>
2025-02-04 07:20:46 -0500

    mesh splitter compatibility patch

---
+++ SkinManagerMod/Patches/LocoMeshSplitterPatches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SkinManagerMod.Patches
+{
+    [HarmonyPatch]
+    internal static class LocoMeshSplitterPatches
+    {
+        private static Type _paintSetupClass = AccessTools.TypeByName("LocoMeshSplitter.MeshLoaders.TrainCarPaintSetup");
+
+        public static bool Prepare()
+        {
+            return (_paintSetupClass is not null);
+        }
+
+        public static IEnumerable<MethodBase> TargetMethods()
+        {
+            if (_paintSetupClass is not null)
+            {
+                var methods = AccessTools.GetDeclaredMethods(_paintSetupClass);
+                foreach (var method in methods)
+                {
+                    yield return method;
+                }
+            }
+        }
+
+        [HarmonyPrefix]
+        public static bool SkipMethod()
+        {
+            return false;
+        }
+    }
+}
