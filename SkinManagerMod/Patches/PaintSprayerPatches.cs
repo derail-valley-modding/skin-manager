02e4724c4ae9861af8123fc89685ff15fbb2d2ff
CruzerBlade9369 <pradiparasendrya@gmail.com>
2025-04-27 01:10:11 +0800

    b99.4 compat fix
    fixed reloadable sockets patches to handle item containers, paint sprayer patch now use magazine system. attempted to fix SM primer not actually functioning as primer and glossiness issues on base materials without metallic maps

--- SkinManagerMod/Patches/PaintSprayerPatches.cs
+++ SkinManagerMod/Patches/PaintSprayerPatches.cs
@@ -3,6 +3,7 @@ using DV.Interaction;
 using DV.InventorySystem;
 using DV.Items;
 using HarmonyLib;
+using UnityEngine;
 
 namespace SkinManagerMod.Patches
 {
@@ -27,23 +28,31 @@ namespace SkinManagerMod.Patches
                 target.CurrentTheme = ___insertedCan.theme;
             }
             __instance.UnUse();
+            __instance.ignoreMagazineChange = true;
+            __instance.magazine.RemoveItem(0, activateItem: true, dropItem: true);
+            __instance.ignoreMagazineChange = false;
 
             // empty can
             if (paintCan.emptyCanPrefab != null)
             {
-                Reloadable reloadable = UnityEngine.Object.Instantiate(paintCan.emptyCanPrefab);
-                RespawnOnDrop component = reloadable.GetComponent<RespawnOnDrop>();
-                if (component != null)
+                GameObject emptyCan = Object.Instantiate(paintCan.emptyCanPrefab, __instance.unloadAnchor.transform.position, __instance.unloadAnchor.transform.rotation).gameObject;
+                RespawnOnDrop comp = emptyCan.GetComponent<RespawnOnDrop>();
+                if (comp != null)
                 {
-                    component.respawnOnDropThroughFloor = false;
-                    component.ignoreDistanceFromSpawnPosition = true;
+                    comp.respawnOnDropThroughFloor = false;
+                    comp.ignoreDistanceFromSpawnPosition = true;
                 }
-                reloadable.GetComponent<InventoryItemSpec>().BelongsToPlayer = true;
-                __instance.socket.Inserted = reloadable;
+                emptyCan.GetComponent<InventoryItemSpec>().BelongsToPlayer = true;
+                __instance.ignoreMagazineChange = true;
+                __instance.magazine.AddItem(emptyCan, 0);
+                __instance.ignoreMagazineChange = false;
+                __instance.insertedCan = emptyCan.GetComponent<PaintCan>();
+                __instance.paintSprayerEffects.OnInserted(emptyCan.transform, playSound: false);
+                __instance.paintSprayerEffects.OnSpent();
             }
             else
             {
-                __instance.socket.Inserted = null;
+                __instance.insertedCan = null;
             }
             Inventory.Instance.DestroyItem(paintCan.gameObject);
 
